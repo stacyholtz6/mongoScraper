f36add7544265d5f2a09a0f789fbf0ba32718978
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-11 19:11:52 -0700

    finished routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cheerio = require('cheerio');
 
 var db = require('./models');
 
-var PORT = 3000;
+var PORT = 8080;
 
 var app = express();
 
@@ -27,36 +27,47 @@ mongoose.connect('mongodb://localhost/articleScraper', {
 
 // GET route for scraping - scrape works - need summary
 app.get('/scrape', function(req, res) {
-  axios.get('https://css-tricks.com/').then(function(response) {
+  axios.get('https://www.reviewjournal.com/').then(function(response) {
     var $ = cheerio.load(response.data);
 
-    $('div.article-article h2').each(function(i, element) {
+    $('div.entry-title').each(function(i, element) {
       var result = {};
 
       result.title = $(this).text();
-      // come back to the summary later
-      // var summary = $('div.article-content p').text();
 
       result.link = $(this)
         .children()
         .attr('href');
 
+      // var summary = $('div.article-content p').text();
+      result.summary = $(this).parent();
+      // .next()
+      // .hasClass('field-excerpt')
+      // .closest('p')
+      // .children()
+      // .attr('div')
+      // .parent()
+      // .attr('p')
+      // .find('p')
+      // .parent()
+      // .children()
+      // .text();
+
       db.Article.create(result)
         .then(function(dbArticle) {
-          console.log(dbArticle);
+          console.log(result);
         })
         .catch(function(err) {
-          console.log(err);
+          //console.log(err);
         });
     });
-
-    // console.log(results);
+    // console.log($.html());
+    // console.log(result);
     res.send('Scrape Complete');
   });
 });
 
-
-// Route for getting all articles from the DB - WORKS!! ✅ 
+// Route for getting all articles from the DB - WORKS!! ✅
 app.get('/articles', function(req, res) {
   db.Article.find({})
     .then(function(dbArticle) {
@@ -68,16 +79,27 @@ app.get('/articles', function(req, res) {
 });
 
 // Route for grabbing a specific article by id, populate it with it's note
-
-
-
-
+app.get('/articles/:id', function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate('note')
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 // Route for saving/updateing an article's associated note
-
-
-
-
+app.post('/articles/:id', function(req, res) {
+  db.Note.create(req.body).then(function(dbNote) {
+    return db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { note: dbNote._id },
+      { new: true }
+    );
+  });
+});
 
 // Start the server
 app.listen(PORT, function() {
