8bf26527aef04298e79f1b535f292ed550e4b4a9
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-12 11:31:47 -0700

    finished routes

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/articleScraper', {
-  useNewUrlParser: true
-});
+// mongoose.connect('mongodb://localhost/articleScraper', {
+//   useNewUrlParser: true
+// });
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/articleScraper';
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
